bc8d4cffe0619833c714fcf4eabb737c88b8aa97
MohamedElmdary <engm5081@gmail.com>
2023-08-30 13:52:14 +0300

    Update terms and condition loading

--- src/components/TermsDialog.vue
+++ src/components/TermsDialog.vue
@@ -1,24 +1,35 @@
 <template>
-  <v-dialog model-value @update:model-value="close" fullscreen>
-    <v-card class="h-100" v-if="!loadedTerms">
-      <v-card-text class="h-100 w-100 d-flex justify-center align-center">
-        <v-progress-circular indeterminate color="primary" size="50" />
-      </v-card-text>
-    </v-card>
+  <v-dialog model-value @update:model-value="close">
+    <div :style="{ height: 'calc(100vh - 40px)', width: 'calc(100vw - 40px)' }">
+      <v-card class="h-100" v-if="!loadedTerms">
+        <v-card-text class="h-100 w-100 d-flex justify-center align-center">
+          <div class="d-flex flex-column align-center">
+            <v-progress-circular indeterminate color="primary" size="50" />
+            <p class="mt-2 font-weight-bold text-subtitle-1">Loading Terms and conditions...</p>
+          </div>
+        </v-card-text>
+      </v-card>
 
-    <iframe
-      v-show="loadedTerms"
-      src="https://library.threefold.me/info/legal/#/"
-      frameborder="0"
-      allow="fullscreen"
-      height="95%"
-      width="100%"
-      sandbox="allow-forms allow-modals allow-scripts allow-popups allow-same-origin"
-      @load="loadedTerms = true"
-    />
-    <v-btn v-show="loadedTerms" block class="font-weight-bold rounded-0" @click="accept">
-      Accept terms and conditions
-    </v-btn>
+      <iframe
+        v-show="loadedTerms"
+        src="https://library.threefold.me/info/legal/#/"
+        frameborder="0"
+        allow="fullscreen"
+        sandbox="allow-forms allow-modals allow-scripts allow-popups allow-same-origin"
+        @load="loadedTerms = true"
+        class="w-100"
+        :style="{ height: 'calc(100% - 50px)' }"
+      />
+      <v-btn
+        v-show="loadedTerms"
+        block
+        class="font-weight-bold rounded-0"
+        :style="{ height: '50px' }"
+        @click="accept"
+      >
+        Accept terms and conditions
+      </v-btn>
+    </div>
   </v-dialog>
 </template>
 
